7685ae724a06c7216673625e71ad4e31b7aff021
Yvo Romp <yvoromp@live.nl>
2020-05-12 15:15:10 +0200

    First commit

---
+++ src/main/java/com/polygon2/BalljamesApplication.java
@@ -0,0 +1,13 @@
+package com.polygon2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalljamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalljamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polygon2/BalljamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polygon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalljamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
